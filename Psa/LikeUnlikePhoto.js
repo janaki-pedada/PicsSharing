9698e62f4fe785ecd2f46714ebdb741cf000c016
janaki-pedada <137858245+janaki-pedada@users.noreply.github.com>
2023-08-13 10:25:23 +0530

    LikeUnlikePhoto.js

--- Psa/LikeUnlikePhoto.js
+++ Psa/LikeUnlikePhoto.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const LikeUnlikePhoto = ({ photo, onLike }) => {
   const handleLike = (event) => {
-    event.stopPropagation(); // Prevent click event from propagating to the parent element (photo grid)
-    onLike(photo.id, !photo.liked); // Inform the parent component that a photo is liked/unliked
+    event.stopPropagation(); 
+    onLike(photo.id, !photo.liked); 
   };
 
   return (
